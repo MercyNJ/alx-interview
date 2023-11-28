22a89fecdf4a0e4a54401fba30fc083070e95128
MercyNJ <mercinjuguna@gmail.com>
2023-11-28 15:05:10 +0000

    Pascal triangle implementation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Pascal's triangle module.
+"""
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle for n rows.
+    """
+
+    triangle = []
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+
