536bd566e5b6c93460daf18ba821f5c117a0f856
MercyNJ <mercinjuguna@gmail.com>
2023-12-11 13:03:49 +0000

    Logical question interview preparation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function that solves the following:
+n a text file, there is a single character H. Your text editor
+can execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewest number
+of operations needed to result in exactly n H characters in the file.
+
+    Prototype: def minOperations(n)
+    Returns an integer
+    If n is impossible to achieve, return 0
+"""
+
+def minOperations(n):
+    """
+    Calculates fewest number of operations.
+    """
+
+    if n <= 1:
+        return 0
+
+    num_operations = 0
+    initial_count = 1
+
+
+
