067520f68f43160dfb76c177b22b78e2d3769244
MercyNJ <mercinjuguna@gmail.com>
2024-01-16 02:24:10 -0800

    correction on styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,17 +9,18 @@ function getCharactersForMovie (movieId) {
 
   request(movieUrl, { json: true }, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      console.error('Error fetching movie information:', error || response.statusCode);
+      console.error('Error:', error || response.statusCode);
       return;
     }
     const characterUrls = body.characters;
 
     characterUrls.forEach((characterUrl) => {
-      request(characterUrl, { json: true }, (charError, charResponse, charBody) => {
+      request(characterUrl, { json: true }, (
+        charError, charResponse, charBody) => {
         if (!charError && charResponse.statusCode === 200) {
           console.log(charBody.name);
         } else {
-          console.error('Error fetching character information:', charError || charResponse.statusCode);
+          console.error('Error:', charError || charResponse.statusCode);
         }
       });
     });
@@ -29,7 +30,7 @@ function getCharactersForMovie (movieId) {
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.error('Please provide a movie ID as a command line argument.');
+  console.error('movie ID required');
 } else {
   getCharactersForMovie(movieId);
 }
