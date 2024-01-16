bb8bc8aac688dd14f8165e3648929e18cde898cb
MercyNJ <mercinjuguna@gmail.com>
2024-01-16 02:32:45 -0800

    correction on style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,33 +4,41 @@ const request = require('request');
 
 const swapiBaseUrl = 'https://swapi-api.alx-tools.com/api/';
 
-function getCharactersForMovie (movieId) {
+function fetchMovie (movieId, callback) {
   const movieUrl = `${swapiBaseUrl}films/${movieId}/`;
 
-  request(movieUrl, { json: true }, (error, response, body) => {
+  request(movieUrl, (error, response, body) => {
     if (error || response.statusCode !== 200) {
-      console.error('Error:', error || response.statusCode);
+      console.error('Error fetching movie information:', error || response.statusCode);
       return;
     }
-    const characterUrls = body.characters;
 
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, { json: true }, (
-        charError, charResponse, charBody) => {
-        if (!charError && charResponse.statusCode === 200) {
-          console.log(charBody.name);
-        } else {
-          console.error('Error:', charError || charResponse.statusCode);
-        }
-      });
-    });
+    const parsedBody = JSON.parse(body);
+    const characterUrls = parsedBody.characters;
+    callback(characterUrls);
+  });
+}
+
+function fetchCharacter (characterUrl) {
+  request(characterUrl, (charError, charResponse, charBody) => {
+    const parsedCharBody = JSON.parse(charBody);
+
+    if (!charError && charResponse.statusCode === 200) {
+      console.log(parsedCharBody.name);
+    } else {
+      console.error('Error fetching character information:', charError || charResponse.statusCode);
+    }
   });
 }
 
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.error('movie ID required');
+  console.error('Please provide a movie ID as a command line argument.');
 } else {
-  getCharactersForMovie(movieId);
+  fetchMovie(movieId, (characterUrls) => {
+    characterUrls.forEach((characterUrl) => {
+      fetchCharacter(characterUrl);
+    });
+  });
 }
