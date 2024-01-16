3f197f0a6a7a49f2cc2cb4d863fc3c426bf95e78
MercyNJ <mercinjuguna@gmail.com>
2024-01-16 03:41:08 -0800

    correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,15 +6,26 @@ const movieId = process.argv[2];
 const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 function fetchMovieCharacters (characters) {
-  characters.forEach(characterUrl => {
+  let processedCharacters = 0;
+
+  function processCharacter (characterUrl) {
     request(characterUrl, (error, response, body) => {
       if (error) {
         console.log(error);
       } else {
         console.log(JSON.parse(body).name);
       }
+
+      processedCharacters++;
+
+      if (processedCharacters === characters.length) {
+        // All characters processed
+
+      }
     });
-  });
+  }
+
+  characters.forEach(processCharacter);
 }
 
 request(movieUrl, (error, response, body) => {
